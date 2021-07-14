5f22d62640368a3ebc471fe93ed71995624b7be8
shahriar <shahriareshraghi22@Gmail.com>
2021-07-14 21:46:38 +0430

    create random number and show message that is guessed number is ok ro not

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 'use strict';
-document.querySelector('.button').addEventListener('click', function () {
-
-})
+// CREATED RANDOM NUMBER    
+const createRandomNumber = function () {
+    return Math.floor(Math.random() * 20)
+}
+const randomNum = createRandomNumber();
+const checkBtn = document.querySelector('.check');
+checkBtn.addEventListener('click', function () {
+    let enteredNum = document.querySelector('.guess').value;
+    if (enteredNum < randomNum) {
+        document.querySelector('.message').textContent = "Too Low :(";
+    } else if (enteredNum > randomNum) {
+        document.querySelector('.message').textContent = "Too High :(";
+    } else {
+        document.querySelector('.message').textContent = "That's it :D";
+    }
+});
